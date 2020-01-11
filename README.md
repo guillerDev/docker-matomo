01a16220f362e8c1b57b4bcef2182eb22920d42a
guillermo <guilleponfe@gmail.com>
2020-01-11 15:30:01 +0100

    Fixed some issues, and included init matomo script.

---
+++ README.md
@@ -0,0 +1,15 @@
+
+
+
+## Start
+
+  git submodules init
+  git submodules update
+
+  ./init-matomo.sh
+
+
+### Docker-compose files:
+
+ - docker-compose.matomo.yml: matomo configuration
+ - docker-compose.override.yml: nginx+certbot configuration
---
+++ init-matomo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NGINX_PATH="./nginx-certbot"
+
+while getopts d:e: option
+  do
+    case "${option}"
+    in
+    d) DOMAIN=${OPTARG};;
+    e) EMAIL=${OPTARG};;
+  esac
+done
+
+cd "$NGINX_PATH"
+./init-letsencrypt.sh -d $DOMAIN -e $EMAIL
+
+echo "### Saving old nginx conf."
+mv  ./data/nginx ./data/nginx-OLD
+echo "### Copying nginx matomo configuration."
+cp -r ../data/nginx ./data
+
+./init-nginxconf.sh -d
+
+echo "### Copying matomo docker-compose."
+cp ../db.env .
+cp ../docker-compose.matomo.yml .
+
+echo "### Starting services."
+docker-compose -f docker-compose.matomo.yml -f docker-compose.override.yml up -d
